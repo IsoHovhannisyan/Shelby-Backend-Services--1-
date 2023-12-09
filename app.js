ee4c57ba1637a4ee0603b9226f30d99d47d0a3b2
IsoHovhannisyan <143291832+IsoHovhannisyan@users.noreply.github.com>
2023-12-09 04:34:49 +0400

    cors

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const app = express();
 
 const cors = require('cors');
 app.use(cors({
-  origin: 'http://localhost:3000/',
+  origin: 'http://localhost:3000',
+  credentials: true
 }));
 
 app.use(logger('dev'));
